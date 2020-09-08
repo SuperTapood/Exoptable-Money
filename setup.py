8bb199f439edb0098318b16cc154d9bbe025402e
SuperTapood <yoav.o252@gmail.com>
2020-09-08 21:29:24 +0300

    updated build creation script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import cx_Freeze
 OPTIONS = {"build_exe": {"packages": ["pygame"],
                          "include_files": ["sprites", "setting.py", "sprite_manager.py",
                          "buy.py", "collision.py", "colors.py", "complex.py",
-                         "fallables.py", "game.py", "group.py", "letters.py", "objects.py", "scenes.py"]}}
+                         "fallables.py", "game.py", "group.py", "main.py", "letters.py", "objects.py", "scenes.py"]}}
 EXECUTABLES = [cx_Freeze.Executable("main.py")]
 
 
